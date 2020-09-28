63c35700f0637ea6e5a677e4347443f2962dc458
Pozadr <adpozor@gmail.com>
2020-09-28 19:57:50 +0200

    Shift -> encode; decode

--- Encryption-Decryption/task/src/encryptdecrypt/Shift.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Shift.java
@@ -8,12 +8,46 @@ public class Shift extends Cipher {
 
     @Override
     public String encode() {
-        return null;
+        StringBuilder result = new StringBuilder();
+
+        for (char ch : data.toCharArray()) {
+            int val = ch;
+            if (val >= 65 && val <= 90) {
+                val = val + key;
+                if (val > 90) {
+                    val = 64 + (val % 90);
+                }
+            } else if (val >= 97 && val <= 122) {
+                val = val + key;
+                if (val > 122) {
+                    val = 96 + (val % 122);
+                }
+            }
+            result.append((char) val);
+        }
+        return result.toString();
     }
 
     @Override
     public String decode() {
-        return null;
+        StringBuilder result = new StringBuilder();
+
+        for (char ch : data.toCharArray()) {
+            int val = ch;
+            if (val >= 65 && val <= 90) {
+                val = val - key;
+                if (val < 65) {
+                    val = 91 - (65 - val);
+                }
+            } else if (val >= 97 && val <= 122) {
+                val = val - key;
+                if (val < 97) {
+                    val = 123 - (97 - val);
+                }
+            }
+            result.append((char) val);
+        }
+        return result.toString();
     }
 
     @Override
